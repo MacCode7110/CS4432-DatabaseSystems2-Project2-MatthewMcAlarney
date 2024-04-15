a56eaa4c1e8fd24ae4f40be136d2b486816cf25e
Matthew McAlarney <67719286+MacCode7110@users.noreply.github.com>
2024-04-15 19:02:37 -0400

    Implemented initializeHashIndex method.

--- RecordRetriever.java
+++ RecordRetriever.java
@@ -1,7 +1,15 @@
+import java.io.FileNotFoundException;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 import java.util.Hashtable;
 import java.lang.StringBuilder;
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class RecordRetriever {
+    private final static int TOTALNUMBEROFFILES = 99;
     private boolean indexesCreated;
     private Hashtable<Integer, StringBuilder> hashIndex;
     private StringBuilder[] arrayIndex;
@@ -16,16 +24,37 @@ public class RecordRetriever {
     public boolean isIndexesCreated() {
         return indexesCreated;
     }
+    public void initializeHashIndex() throws IOException {
+        //Note: A record location in a StringBuilder object is represented using the following notation: File Number (F + #) + "-" + Record Offset (starting index of the record location in the file).
+        //A dash is used to separate multiple locations of a record in a given file.
+        //Example StringBuilder object contents: F1-0-40-80
+        this.hashIndex = new Hashtable<Integer, StringBuilder>();
+        String filePath;
+        FileInputStream fileInputStream;
+        byte[] record;
+        int randomVValue;
+        ByteBuffer bytes = ByteBuffer.allocate(40);
+        for (int i = 0; i < TOTALNUMBEROFFILES; i++) {
+            filePath = "Project2Dataset/F" + (i + 1) + ".txt";
+            fileInputStream = new FileInputStream(filePath);
+            for (int j = 0; j < 4000; j+= 40) {
+                fileInputStream.getChannel().read(bytes, j);
+                record = bytes.array();
+                randomVValue = Integer.parseInt(new String(Arrays.copyOfRange(record, j + 33, j + 37)));
 
-    public void initializeHashIndex() {
-        //A hash table entry should have two
-        //components (key k, value v), where k = RandomV value, and v = the record locations (file number and the offset at
-        //which the record begins within this file).
+                if (!(this.hashIndex.contains(randomVValue))) {
+                    this.hashIndex.put(randomVValue, new StringBuilder("F" + (i + 1) + "-" + j));
+                } else {
+                    this.hashIndex.put(randomVValue, hashIndex.get(randomVValue).append("-" + j));
+                }
 
+                bytes.clear();
+            }
+        }
     }
 
     public void initializeArrayIndex() {
-
+        this.arrayIndex = new StringBuilder[5000];
     }
     public void handleEqualityQueryLookup(int v) {
 
